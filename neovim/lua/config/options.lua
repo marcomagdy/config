5cef41b8ba0ec0537c09c2bb2da7d7b82f325075
Marco Magdy <mmagdy@gmail.com>
2026-04-01 10:18:05 -0700

    [vim] plugin update

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -7,9 +7,10 @@ vim.g.lazyvim_picker = "fzf"
 vim.opt.textwidth = 120
 vim.opt.colorcolumn = "120"
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4 -- Number of spaces for each indent level
+vim.opt.tabstop = 4 -- How many columns a tab character displays as
+vim.opt.softtabstop = 4 -- Number of spaces inserted when you press Tab
 
 -- Don't copy all the yanked text to the system clipboard
 vim.opt.clipboard = ""
--- neovim/lua/plugins/camelcase.lua
+++ neovim/lua/plugins/camelcase.lua
@@ -1,7 +1,17 @@
 return {
   "bkad/CamelCaseMotion",
   init = function()
-    vim.keymap.set("n", "<S-W>", "<Plug>CamelCaseMotion_w", { silent = true, desc = "CamelCaseMotion fwd" })
-    vim.keymap.set("n", "<S-B>", "<Plug>CamelCaseMotion_b", { silent = true, desc = "CamelCaseMotion back" })
+    vim.keymap.set(
+      { "n", "o", "v" },
+      "<S-W>",
+      "<Plug>CamelCaseMotion_w",
+      { silent = true, desc = "CamelCaseMotion fwd" }
+    )
+    vim.keymap.set(
+      { "n", "o", "v" },
+      "<S-B>",
+      "<Plug>CamelCaseMotion_b",
+      { silent = true, desc = "CamelCaseMotion back" }
+    )
   end,
 }
